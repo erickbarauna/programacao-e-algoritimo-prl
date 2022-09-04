84414380b66e8555c0a709f6767a187da7c05a79
Erick Barauna <98670029+erickbarauna@users.noreply.github.com>
2022-09-04 08:17:08 -0300

    formatacao de texto e expressoes booleanas

---
+++ Estudos_Livres_Relacionados/formatacao-de-texto_expressoes-booleanas/formatacao-de-texto_expressoes-booleanas/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formatacao_de_texto_expressoes_booleanas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            /* Console.WriteLine("a" == "a");  // True
+            Console.WriteLine("a" == "A");  // False
+            Console.WriteLine(1 == 2);  // False
+
+            string myValue = "a";
+            Console.WriteLine(myValue == "a");  // True */
+
+
+            // 2
+            /* Console.WriteLine("a" == "a "); // True */
+
+
+            // 3
+            /* string value1 = " a";
+            string value2 = "A ";
+            Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());  // True */
+
+
+            // 4
+            /* Console.WriteLine("a" != "a");  // False
+            Console.WriteLine("a" != "A");  // True
+            Console.WriteLine(1 != 2);  // True
+
+            string myValue = "a";
+            Console.WriteLine(myValue != "a");  // False */
+
+
+            // 5
+            /* Console.WriteLine(1 > 2);  // False
+            Console.WriteLine(1 < 2);  // True
+            Console.WriteLine(1 >= 1);  // True
+            Console.WriteLine(1 <= 1);  // True */
+
+
+            // 6
+            /* string frase = "Eu não gosto de tomar banho!";
+
+            Console.WriteLine(frase.Contains("não"));  // True 
+            Console.WriteLine(!frase.Contains("não"));  // False */
+
+
+            // 7
+            /*
+            // Exemplo 1
+            int Compra = 1001;
+
+            int Desconto = Compra > 1000 ? Compra - 100 : Compra - 50;
+
+            Console.WriteLine($"Com o desconto aplicado, o valor de sua compra será: {Desconto}");
+
+            // Exemplo 2
+            int Conta = 350;
+
+            Console.WriteLine($"De acordo com o valor da sua conta você receberá um desconto de: {(Conta > 250 ? 30 : 10)}%");
+            */
+        }
+    }
+}
---
+++ Estudos_Livres_Relacionados/formatacao-de-texto_expressoes-booleanas/formatacao-de-texto_expressoes-booleanas/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("formatacao-de-texto_expressoes-booleanas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("formatacao-de-texto_expressoes-booleanas")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("48f3cd66-7a14-4465-bf60-6db0d0e521b2")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
