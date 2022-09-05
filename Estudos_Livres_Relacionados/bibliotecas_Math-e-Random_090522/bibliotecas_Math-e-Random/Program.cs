08e2945556be23e2c0b682c84dabc8743d9e03d7
Erick Barauna <98670029+erickbarauna@users.noreply.github.com>
2022-09-05 12:06:58 -0300

    bibliotecas .NET

---
+++ Estudos_Livres_Relacionados/bibliotecas_Math-e-Random_090522/bibliotecas_Math-e-Random/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bibliotecas_Math_e_Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // RANDOM 
+            /*
+
+            // 01
+            // Exemplo 01
+            Random dado = new Random();
+            int jogada = dado.Next(1, 7);
+            Console.WriteLine(jogada);
+
+            // Exemplo 02
+            Random dado = new Random();
+            Console.WriteLine(dado.Next(1, 7));
+
+
+            // 02
+            Random dado = new Random();
+            int jogada1 = dado.Next();
+            int jogada2 = dado.Next(101);
+            int jogada3 = dado.Next(50, 101);
+
+            Console.WriteLine($"Primiera jogada: {jogada1}");
+            Console.WriteLine($"Segunda jogada: {jogada2}");
+            Console.WriteLine($"Terceira jogada: {jogada3}");
+            */
+
+
+            // MATH
+
+            /*
+            int firstValue = 500;
+            int secondValue = 600;
+            int largerValue = Math.Max(firstValue, secondValue);
+
+            Console.WriteLine(largerValue);
+            */
+        }
+    }
+}
