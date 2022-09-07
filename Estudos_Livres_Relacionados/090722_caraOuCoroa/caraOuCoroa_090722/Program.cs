332b86e5f8a76053b56697bf38812a0a4773e5ea
Joyce Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-07 15:59:44 -0300

    formatando texto e expressoes booleanas

---
+++ Estudos_Livres_Relacionados/090722_caraOuCoroa/caraOuCoroa_090722/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caraOuCoroa_090722
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CARA OU COROA 
+
+            /* Exemplo 01
+            Random moeda = new Random();
+            int jogada = moeda.Next(0, 2);
+            Console.WriteLine(jogada == 0 ? "cara" : "coroa");
+            */
+
+            /* Exemplo 02
+            Random moeda = new Random();
+            Console.WriteLine(moeda.Next(0, 2) == 0 ? "cara" : "coroa");
+            */
+        }
+    }
+}
---
+++ Estudos_Livres_Relacionados/090722_permissoesComplicadas/permissoesComplicadas_090722/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace permissoesComplicadas_090722
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("- Admin \n- Gerente \n- Outro \nDigite uma das opções para continuar: ");
+            Console.SetCursorPosition(38, 3);
+            string entrada = Console.ReadLine();
+
+
+            Console.WriteLine("Digite seu nível [0/100]: ");
+            Console.SetCursorPosition(26, 4);
+            int nivel = int.Parse(Console.ReadLine());
+
+
+            string permissao = entrada.Trim().ToLower();
+            Console.WriteLine();
+
+
+            if (permissao.Contains("admin"))
+            {
+                if (nivel > 55)
+                {
+                    Console.WriteLine("Bem-vindo, usuário superadministrador.");
+                }
+                else
+                {
+                    Console.WriteLine("Bem-vindo, usuário administrador.");
+                }
+            }
+            else if (permissao.Contains("gerente"))
+            {
+                if (nivel >= 20)
+                {
+                    Console.WriteLine("Entre em contato com um administrador para obter acesso.");
+                }
+                else
+                {
+                    Console.WriteLine("Você não tem privilégios suficientes.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Você não tem privilégios suficientes.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
