8f0dc9109c8546ef632bf116b75a5362b0e28b54
Joyce Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-09 08:28:21 -0300

    Ex015a - feito em sala

---
+++ Exercicios_PRL/FASE02/Ex015a_PRL_090822/Ex015_PRL_090822/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex015_PRL_090822
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int Numero;  // Variável inteira - entrada
+
+            Console.Clear();  // Limpa tela
+            Console.WriteLine("Digite um número: ");  // Interface 1
+            Console.SetCursorPosition(18, 0);  // Posição 1
+            Numero = int.Parse(Console.ReadLine());  // Entrada 1
+
+            if (Numero > 50)  // Condicional 1
+            {
+                if (Numero <= 90)  // Condicional 2
+                {
+                    Console.WriteLine("Número: " + Numero);  // Saída 1
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
