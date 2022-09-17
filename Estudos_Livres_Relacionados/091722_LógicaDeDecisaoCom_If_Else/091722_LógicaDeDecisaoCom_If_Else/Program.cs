5e5d32fc2efe5fee23a5770f6f64ceecc981fe03
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-17 08:44:20 -0300

    aprimorando conhecimentos IF ELSE

---
+++ Estudos_Livres_Relacionados/091722_LógicaDeDecisaoCom_If_Else/091722_LógicaDeDecisaoCom_If_Else/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _091722_LógicaDeDecisaoCom_If_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ROLA 3 DADOS E EXIBE A PONTUAÇÃO DE ACORDO COM AS REGRAS
+            /*01
+            Random dado = new Random();
+         
+            int rolagem1 = dado.Next(1, 7);
+            int rolagem2 = dado.Next(1, 7);
+            int rolagem3 = dado.Next(1, 7);
+
+            int total = rolagem1 + rolagem2 + rolagem3;
+
+            Console.WriteLine($"Resultado dos 3 dados rolados: {rolagem1}, {rolagem2}, {rolagem3}");
+            Thread.Sleep(2000);
+
+            if (rolagem1 == rolagem2 && rolagem1 == rolagem3)
+            {
+                Console.WriteLine("Você rolou 3 dados IGUAIS! +6 de bônus no total!");
+                total += 6;
+                Thread.Sleep(2500);
+            }
+            else if (rolagem1 == rolagem2 || rolagem1 == rolagem3 || rolagem2 == rolagem3)
+            {
+                Console.WriteLine("Você rolou 2 dados iguais! +2 de bônus no total!");
+                total += 2;
+                Thread.Sleep(2500);
+            }
+            
+            Console.WriteLine($"\nTotal: {total}");
+            Thread.Sleep(1300);
+
+            Console.WriteLine(total >= 15 ? "\nPARABÉNS! VOCÊ GANHOU!" : "\nDesculpe Você Perdeu.");
+            Console.ReadLine();
+            */
+        }
+    }
+}
