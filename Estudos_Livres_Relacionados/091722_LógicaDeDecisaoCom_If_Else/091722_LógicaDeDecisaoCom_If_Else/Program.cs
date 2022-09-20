75a232ce2c54032021e45907cf4ea222d5861a8e
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-20 08:32:16 -0300

    fim do módulo de if / else if/ else

--- Estudos_Livres_Relacionados/091722_LógicaDeDecisaoCom_If_Else/091722_LógicaDeDecisaoCom_If_Else/Program.cs
+++ Estudos_Livres_Relacionados/091722_LógicaDeDecisaoCom_If_Else/091722_LógicaDeDecisaoCom_If_Else/Program.cs
@@ -11,8 +11,8 @@ namespace _091722_LógicaDeDecisaoCom_If_Else
     {
         static void Main(string[] args)
         {
-            // ROLA 3 DADOS E EXIBE A PONTUAÇÃO DE ACORDO COM AS REGRAS
-            /*01
+            // ROLA 3 DADOS E EXIBE A PONTUAÇÃO DE ACORDO COM AS REGRAS E GANHA PRÊMIOS
+            /*
             Random dado = new Random();
          
             int rolagem1 = dado.Next(1, 7);
@@ -28,21 +28,73 @@ namespace _091722_LógicaDeDecisaoCom_If_Else
             {
                 Console.WriteLine("Você rolou 3 dados IGUAIS! +6 de bônus no total!");
                 total += 6;
-                Thread.Sleep(2500);
+                Thread.Sleep(2600);
             }
             else if (rolagem1 == rolagem2 || rolagem1 == rolagem3 || rolagem2 == rolagem3)
             {
                 Console.WriteLine("Você rolou 2 dados iguais! +2 de bônus no total!");
                 total += 2;
-                Thread.Sleep(2500);
+                Thread.Sleep(2600);
             }
             
-            Console.WriteLine($"\nTotal: {total}");
-            Thread.Sleep(1300);
 
-            Console.WriteLine(total >= 15 ? "\nPARABÉNS! VOCÊ GANHOU!" : "\nDesculpe Você Perdeu.");
+            Console.WriteLine($"\nTotal: {total}");
+            Thread.Sleep(2000);
+
+
+            if (total > 15)
+            {
+                Console.WriteLine("Você ganhou um CARRO!");
+            }
+            else if (total > 9)
+            {
+                Console.WriteLine("Você ganhou um LAPTOP!");
+            }
+            else if (total == 7)
+            {
+                Console.WriteLine("Você ganhou uma VIAGEM!");
+            }
+            else
+            {
+                Console.WriteLine("Você ganhou um GATINHO!");
+            }
+
             Console.ReadLine();
             */
+
+
+            // APRIMORAMENTO DE TAXA DE RENOVAÇÃO DE ASSINATURAS
+
+            Random dia = new Random();
+            int diasAteVencimento = dia.Next(12);  // Gera um n° aleatório que será o tempo de assinatura
+            int percentualDeDesconto = 0;  // Porcentagem que será aplicada se a assinatura for renovada de acordo com os dias restantes
+
+
+            if (diasAteVencimento == 0)
+            {
+                Console.WriteLine("Sua assinatura expirou.");
+            }
+            else if (diasAteVencimento == 1)
+            {
+                percentualDeDesconto = 20;
+                Console.WriteLine($"Sua assinatura expira em {diasAteVencimento} dia!");
+            }
+            else if (diasAteVencimento < 6)
+            {
+                percentualDeDesconto = 10;
+                Console.WriteLine($"Sua assinatura expira em {diasAteVencimento}.");
+            }
+            else if (diasAteVencimento < 11)
+            {
+                Console.WriteLine("Sua assinatura expirará em breve. Renove agora!");
+            }
+
+            if (percentualDeDesconto > 0)
+            {
+                Console.WriteLine($"Renove agora e economize {percentualDeDesconto}%.");
+            }
+
+            Console.ReadLine(); 
         }
     }
 }
