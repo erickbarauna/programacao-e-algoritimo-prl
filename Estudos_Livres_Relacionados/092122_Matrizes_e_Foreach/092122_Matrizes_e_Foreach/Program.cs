a2c5c270d558837234577916251daefe10d0a905
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-21 07:31:30 -0300

    exercícios do dia criados

--- Estudos_Livres_Relacionados/092122_Matrizes_e_Foreach/092122_Matrizes_e_Foreach/Program.cs
+++ Estudos_Livres_Relacionados/092122_Matrizes_e_Foreach/092122_Matrizes_e_Foreach/Program.cs
@@ -10,6 +10,71 @@ namespace _092122_Matrizes_e_Foreach
     {
         static void Main(string[] args)
         {
+            // NOÇÕES BÁSICAS DE MATRIZ
+            /*01
+            string[] fraudulentOrderIDs = new string[3];
+
+            fraudulentOrderIDs[0] = "A123";
+            fraudulentOrderIDs[1] = "B456";
+            fraudulentOrderIDs[2] = "C789";
+            // fraudulentOrderIDs[3] = "D000";
+
+            Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+            Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+            Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+            fraudulentOrderIDs[0] = "F000";
+
+            Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+            Console.ReadLine();
+            */
+
+            /*02
+            string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+            Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+            Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+            Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+            fraudulentOrderIDs[0] = "F000";
+
+            Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+            Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+            Console.ReadLine();
+            */
+
+
+            // INSTRUÇÃO FOREACH
+            /*01
+            int numero = 0;
+
+            string[] names = { "Bob", "Conrad", "Grant" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name + $" {names[numero]}");
+                numero++;
+            }
+
+            Console.ReadLine();
+            */
+
+            /*
+            int[] inventory = { 200, 450, 700, 175, 250 };
+            int sum = 0;
+            int bin = 0;
+            foreach (int items in inventory)
+            {
+                sum += items;
+                bin++;
+                Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+            }
+            Console.WriteLine($"We have {sum} items in inventory.");
+
+            Console.ReadLine();
+            */
         }
     }
 }
