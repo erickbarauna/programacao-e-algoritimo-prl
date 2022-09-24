60454a86ef6c28d7c12b6f7408f50fbe5cd73473
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-24 07:27:55 -0300

    conteudo de exercicos finalizado

---
+++ Estudos_Livres_Relacionados/092422_CodigoLegivel/exercicios/092422_CodigoLegivel/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _092422_CodigoLegivel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Bob";
+            int widgetsPurchased = 7;
+            // Testing a change to the message.
+            // int widgetsSold = 7;
+            // Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+            Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+
+
+            /* 
+              This is a long comment 
+              that spans multiple lines
+              just to prove that it can
+              be done.
+            */
+
+
+
+
+            /*
+              The following code creates five random OrderIDs
+              to test the fraud detection process.  OrderIDs 
+              consist of a letter from A to E, and a three
+              digit number. Ex. A123.
+            */
+            Random random = new Random();
+            string[] orderIDs = new string[5];
+
+            for (int i = 0; i < orderIDs.Length; i++)
+            {
+                int prefixValue = random.Next(65, 70);
+                string prefix = Convert.ToChar(prefixValue).ToString();
+                string suffix = random.Next(1, 1000).ToString("000");
+
+                orderIDs[i] = prefix + suffix;
+            }
+
+            foreach (var orderID in orderIDs)
+            {
+                Console.WriteLine(orderID);
+            }
+        }
+    }
+}
