2404b80082b0400d0ea19ad89dcf1fb11d6f5ba3
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-24 07:44:29 -0300

    desafio concluido

---
+++ Estudos_Livres_Relacionados/092422_CodigoLegivel/desafio/092422_desafio/092422_desafio/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _092422_desafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Neste desafio, você usará as técnicas aprendidas neste módulo.
+                Seu objetivo é fazer melhorias em um código mal formatado e mal comentado
+                para melhorar sua legibilidade. 
+            */
+
+
+
+            /*
+                O código abaixo inverte uma cadeia de caracteres,
+                procura a quantidade de vezes que aparecem a letra "o"
+                e imprime uma mensagem com o resultado na tela. 
+            */
+
+            string str = "A rápida raposa marrom pula sobre o cachorro preguiçoso.";
+            
+            char[] charMessage = str.ToCharArray();
+            Array.Reverse(charMessage);
+
+            int contador = 0;
+            
+            foreach (char valor in charMessage)
+            {
+                if (valor == 'o')
+                { 
+                    contador++;
+                } 
+            }
+            
+            string new_message = new String(charMessage);
+            
+            Console.WriteLine(new_message);
+            Console.WriteLine($"\nA letra 'o' aparece {contador} vezes.");
+
+
+            Console.ReadLine();
+        }
+    }
+}
