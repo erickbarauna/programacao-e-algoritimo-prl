0f632c491d39fd436faffd06a6a520dd8128f331
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-24 20:25:42 -0300

    primeiro modulo do roteiro concluido
    roteiro: Adicione lógica de programação aos seus aplicativos com C#

---
+++ Estudos_Livres_Relacionados/092422_EscopoDaVariavel/desafio/092422_desafio/092422_desafio/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _092422_desafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use o que você aprendeu neste módulo para corrigir este código mal escrito.
+            /*
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            foreach (int number in numbers)
+            {
+                int total;
+                total += number;
+                if (number == 42)
+                {
+                    bool found = true;
+                }
+            }
+            if (found)
+            {
+                Console.WriteLine("Set contains 42");
+            }
+            Console.WriteLine($"Total: {total}");
+            Console.ReadLine();
+            */
+
+
+            // RESOLUÇÃO
+
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            int total = 0;
+            bool found = false;
+
+
+            foreach (int number in numbers)
+            {
+                total += number;
+                if (number == 42) found = true;
+            }
+
+            if (found) Console.WriteLine("Set contains 42");
+
+            Console.WriteLine($"Total: {total}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Estudos_Livres_Relacionados/092422_EscopoDaVariavel/exercicios/092422_EscopoDaVariavel/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _092422_EscopoDaVariavel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // BLOCOS DE CÓDIGO AFETAM O ESCOPO DE UMA VARIAVEL 
+
+            /*
+            Os blocos de código definem os limites de estruturas de nível superior,
+            tais como namespaces, classes e métodos, assim como definem os
+            limites das instruções de decisão e de iteração.
+            Esses limites exigem consideração, porque afetam a visibilidade das variáveis
+            e de outras estruturas maiores, assim como métodos e classes.
+            Em alguns casos, palavras - chave adicionais como public e using devem ser usadas
+            para adentrar os limites de outro bloco de código.Em outros casos,
+            as estruturas definidas como irmãos dentro do mesmo bloco de código podem
+            fazer referência umas às outras livremente.
+            */
+
+            /*
+            bool flag = true;
+            if (flag)
+            {
+               
+                //Só é possível utilizar a variável int dentro deste if
+                //Para usa-la fora será preciso declara-la fora do if
+
+                int value = 10;
+
+                Console.WriteLine($"Inside of code block: {value}");
+            } 
+
+            Console.WriteLine($"Outside of code block: {value}");
+            */
+
+
+            // REMOVER BLOCOS DE CÓDIGO EM INSTRUÇÕES IF
+            /* 01
+            bool flag = true;
+            if (flag)
+            {
+                Console.WriteLine(flag);
+            }
+            */
+
+            /*
+            bool flag = true;
+            if (flag)
+                Console.WriteLine(flag);
+            */
+
+            /*
+            bool flag = true;
+            if (flag) Console.WriteLine(flag); 
+            */
+
+
+            /*02
+            string name = "steve";
+            if (name == "bob") Console.WriteLine("Found Bob");
+            else if (name == "steve") Console.WriteLine("Found Steve");
+            else Console.WriteLine("Found Chuck");
+            */
+        }    
+    }
+}
