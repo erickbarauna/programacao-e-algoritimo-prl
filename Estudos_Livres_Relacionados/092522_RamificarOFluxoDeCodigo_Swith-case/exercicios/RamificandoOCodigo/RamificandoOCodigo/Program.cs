06c2d05306a8c5f09d89a9bb9d3097b7e763cd59
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-25 18:59:39 -0300

    exercicios do nodulo finalizados

---
+++ Estudos_Livres_Relacionados/092522_RamificarOFluxoDeCodigo_Swith-case/exercicios/RamificandoOCodigo/RamificandoOCodigo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RamificandoOCodigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INSTRUÇÃO SWITCH CASE
+            /*01
+            int nivelDeFuncio = 200;
+            string nomeDoFuncio = "John Smith";
+
+            string title = "";
+
+            switch (nivelDeFuncio)
+            {
+                case 100:
+                case 200:
+                    title = "Senior Associate";
+                    break;
+                case 300:
+                    title = "Manager";
+                    break;
+                case 400:
+                    title = "Senior Manager";
+                    break;
+                default:
+                    title = "Associate";
+                    break;
+            }
+
+            Console.WriteLine($"{nomeDoFuncio}, {title}");
+            Console.ReadLine();
+            */
+        }
+    }
+}
