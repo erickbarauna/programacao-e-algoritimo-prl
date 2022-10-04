162050e6aa49648d6b660d42d6683ab6ecbf3e97
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-10-04 07:48:43 -0300

    desafio do módulo concluído

---
+++ Estudos_Livres_Relacionados/100222_ExecutarOperacoesEmMatrizesUsandoMet.Aux/desafio/OperacaoEmMatrizes2/OperacaoEmMatrizes2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacaoEmMatrizes2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Segundo desafio
+            /*
+            imprima cada ID de pedido que começa com a letra "B".
+            string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+            A saída deve corresponder ao seguinte:
+
+            B123
+            B177
+            B179
+            */
+
+
+            // Solução
+
+            string orderStream = "B123-C234-A345+C15-B177 G3003-C235-B179";
+            string[] items = orderStream.Split('-', ' ', '+');
+
+            foreach (var item in items)
+            {
+                if (item.StartsWith("B"))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
