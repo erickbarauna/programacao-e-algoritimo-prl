5568f7f6548e29f18e77f472b28bfdbc77901ae2
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-10-04 07:32:14 -0300

    desafio 1 concluído

---
+++ Estudos_Livres_Relacionados/100222_ExecutarOperacoesEmMatrizesUsandoMet.Aux/desafio/OperacoesEmMatrizes/OperacoesEmMatrizes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacoesEmMatrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inverter cada palavra de uma mensagem
+            /*
+            string pangram = "The quick brown fox jumps over the lazy dog";
+
+            Se você tiver êxito, deverá ver a seguinte saída:
+
+            ehT kciuq nworb xof spmuj revo eht yzal god
+            */
+
+
+            // Solução
+
+            string pangram = "The quick brown fox jumps over the lazy dog";
+
+            string[] message = pangram.Split(' ');
+            string[] newMessage = new string[message.Length];
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                char[] letters = message[i].ToCharArray();
+                Array.Reverse(letters);
+                newMessage[i] = new string(letters);
+            }
+
+            string result = String.Join(" ", newMessage);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+         }
+    }
+}
