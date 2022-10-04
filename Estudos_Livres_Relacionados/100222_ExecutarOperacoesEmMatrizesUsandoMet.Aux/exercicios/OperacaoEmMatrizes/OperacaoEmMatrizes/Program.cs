ac701b67431a4a22eca05ec6e7d4720ec1dba881
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-10-04 07:07:20 -0300

    exercicios do módulo finalizados

--- Estudos_Livres_Relacionados/100222_ExecutarOperacoesEmMatrizesUsandoMet.Aux/exercicios/OperacaoEmMatrizes/OperacaoEmMatrizes/Program.cs
+++ Estudos_Livres_Relacionados/100222_ExecutarOperacoesEmMatrizesUsandoMet.Aux/exercicios/OperacaoEmMatrizes/OperacaoEmMatrizes/Program.cs
@@ -90,6 +90,24 @@ namespace OperacaoEmMatrizes
             }
             */
 
+
+            // Split() e Join()
+
+            /*
+            string value = "abc123";
+            char[] valueArray = value.ToCharArray();
+            Array.Reverse(valueArray);
+            // string result = new string(valueArray);
+            string result = String.Join(",", valueArray);
+            Console.WriteLine(result);
+
+            string[] items = result.Split(',');
+            foreach (string item in items)
+            {
+                Console.WriteLine(item);
+            }
+            */
+
             Console.ReadLine();
         }
     }
