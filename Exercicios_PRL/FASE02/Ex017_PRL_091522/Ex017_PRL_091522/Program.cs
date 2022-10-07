6bece3d51f4a65e08784d81cb7c62b2061cf74a1
Joyce Pereira Oliveira <ericksbarauna26@gmail.com>
2022-10-07 07:45:43 -0300

    exercicio 21 finalizado
    Código em que eu mais utilizei meus conhecimentos até agora 07/10/22

--- Exercicios_PRL/FASE02/Ex017_PRL_091522/Ex017_PRL_091522/Program.cs
+++ Exercicios_PRL/FASE02/Ex017_PRL_091522/Ex017_PRL_091522/Program.cs
@@ -10,83 +10,75 @@ namespace Ex017_PRL_091522
     {
         static void Main(string[] args)
         {
-            int n1, n2, n3;  // Varialvel inteira - Entrada
+            string value = ""; // Variavel texto - Entrada
+            double n1, n2, n3; // Variaveis inteiras - Saída
+            int resultado = 0; // Variavel inteira - Saída
 
-            int maior = 0;  // Varialvel inteira - Saída
-            int meio = 0;  // Varialvel inteira - Saída
-            int menor = 0;  // Varialvel inteira - Saída
+            string resposta = "s"; // Variavel texto - Entrada
 
-            try
+            while (resposta == "s") // Condicional 1
             {
                 Console.Clear(); // Limpa tela
-                Console.WriteLine("Digite o 1° número: ");  // Interface 1
-                Console.SetCursorPosition(20, 0);  // Posição 1
-                n1 = int.Parse(Console.ReadLine());  // Entrada 1
+                Console.WriteLine("Digite um número: "); // Interface 1
+                Console.SetCursorPosition(18, 0); // Posição 1
+                value = Console.ReadLine(); // Entrada 1
 
-
-                Console.WriteLine("Digite o 2° número: ");  // Interface 2
-                Console.SetCursorPosition(20, 1);  // Posição 2
-                n2 = int.Parse(Console.ReadLine());  // Entrada 2
-
-
-                Console.WriteLine("Digite o 3° número: ");  // Interface 3
-                Console.SetCursorPosition(20, 2);  // Posição 3
-                n3 = int.Parse(Console.ReadLine());  // Entrada 3
-
-
-                if (n1 != n2 & n1 != n3 & n2 != n3)  // Condicional 1
+                if (int.TryParse(value, out resultado)) // Condicional 2
                 {
-                    if (n1 < n2 & n1 < n3 & n2 < n3)  // Condicional 2
-                    {
-                        menor = n1;  // Processo 1
-                        meio = n2;  // Processo 2
-                        maior = n3;  // Processo 3
-                    }
+                    n1 = (resultado / 3) - 1; // Processo 1
+                    n2 = (n1 + 1); // Processo 2
+                    n3 = n1 + 2; // Processo 3
 
-                    if (n1 < n2 & n1 < n3 & n3 < n2)  // Condicional 3
+                    if (n1 + n2 + n3 < resultado) // Condicional 3
                     {
-                        menor = n1;  // Processo 4
-                        meio = n3;  // Processo 5
-                        maior = n2;  // Processo 6
+                        Console.WriteLine("Não existe soma de números consecutivos para este número digitado!\n"); // saída 1
+                        Thread.Sleep(1500); // Tempo de espera 
                     }
-
-                    if (n2 < n1 & n2 < n3 & n1 < n3)  // Condicional 4
+                    else // Negação de Condicional 1
                     {
-                        menor = n2;  // Processo 7
-                        meio = n1;  // Processo 8
-                        maior = n3;  // Processo 9
+                        Console.Write($"Resultado: {n1}"); // Saída 2
+                        Thread.Sleep(800); // Tempo de espera
+                        Console.Write($" + {n2}"); // Saída 3
+                        Thread.Sleep(800); // Tempo de espera
+                        Console.Write($" + {n3}"); // Saída 4
+                        Thread.Sleep(1000); // Tempo de espera
+                        Console.Write($" = {resultado}\n\n"); // Saída 5
+                        Thread.Sleep(1500); // Tempo de espera
                     }
+                }
+                else // Negação de Condicional 2
+                {
+                    Console.WriteLine("Por favor digite um número para que possa ser testado!\n"); // Saída 6
+                    Thread.Sleep(1500); // Tempo de espera
+                }
 
-                    if (n2 < n1 & n2 < n3 & n3 < n1)  // Condicional 5
+                string resposta2 = ""; // Variavel texto - Entrada
+                int contador = 3; // Variavel inteira - ?
+
+                while (resposta2 != "s") // Condicional 4
+                {
+                    Console.WriteLine("Quer continuar [S/N]? "); // Interface 2
+                    Console.SetCursorPosition(22, contador); // Posição 2
+                    resposta2 = Console.ReadLine(); // Entrada 2
+                    resposta2 = resposta2.ToLower(); // Processo 4
+                    resposta = resposta2; // Processo 5
+                    contador++; // Processo 6
+
+                    if (resposta != "n" && resposta != "s") // Condicional 5
                     {
-                        menor = n2;  // Processo 10
-                        meio = n3;  // Processo 11
-                        maior = n1;  // Processo 12
+                        contador += 2; // Processo 7
+                        Console.WriteLine("\nDigite apenas S ou N!"); // Saída 7
                     }
-
-                    if (n3 < n1 & n3 < n2 & n2 < n1)  // Condicional 6
+                    else if (resposta == "n") // Condicional 6
                     {
-                        menor = n3;  // Processo 13
-                        meio = n1;  // Processo 14
-                        maior = n2;  // Processo 15
+                        Thread.Sleep(700); // Tempo de espera
+                        Console.WriteLine("Encerrando..."); // Saída 8
+                        Thread.Sleep(1800); // Tempo de espera
+                        Environment.Exit(0); // Sair do console
                     }
-
-                    if (n3 < n1 & n3 < n2 & n2 < n1)  // Condicional 7
-                    {
-                        menor = n3;  // Processo 16
-                        meio = n2;  // Processo 17
-                        maior = n1;  // Processo 18
-                    }
-
-                    Console.WriteLine($"Ordem: {menor}, {meio}, {maior}");  // Saída
-                    Console.ReadLine();
                 }
             }
-            catch (Exception)
-            {
-                Console.WriteLine("Digite um número válido!");  // Saída 2
-                Console.ReadLine();
-            }
+            Console.ReadLine();
         }
     }
 }
--- Exercicios_PRL/FASE02/Ex021_PRL_100622/Ex021_PRL_100622/Ex021_PRL_100622/Program.cs
+++ Exercicios_PRL/FASE02/Ex021_PRL_100622/Ex021_PRL_100622/Ex021_PRL_100622/Program.cs
@@ -10,25 +10,27 @@ namespace Ex021_PRL_100622
     {
         static void Main(string[] args)
         {
-            try
+            double n1, n2, n3; // Variaveis inteiras - Saída
+
+            string value = "";
+            int resultado = 0;
+
+            string resposta = "s"; // Variavel texto - Entrada
+
+            while (resposta == "s")
             {
-                int numero; // Variavel inteira - Entrada
-                double n1, n2, n3; // Variaveis inteiras - Saída
+                Console.Clear(); // Limpa tela
+                Console.WriteLine("Digite um número: "); // Interface 1
+                Console.SetCursorPosition(18, 0); // Posição 1
+                value = Console.ReadLine(); // Entrada 1
 
-                string resposta = "s"; // Variavel texto - Entrada
-
-                while (resposta == "s")
+                if (int.TryParse(value, out resultado))
                 {
-                    Console.Clear(); // Limpa tela
-                    Console.WriteLine("Digite um número: "); // Interface 1
-                    Console.SetCursorPosition(18, 0); // Posição 1
-                    numero = int.Parse(Console.ReadLine()); // Entrada 1
-
-                    n1 = (numero / 3) - 1; // Processo 1
+                    n1 = (resultado / 3) - 1; // Processo 1
                     n2 = (n1 + 1); // Processo 2
                     n3 = n1 + 2; // Processo 3
 
-                    if (n1 + n2 + n3 < numero) // Condicional 1
+                    if (n1 + n2 + n3 < resultado) // Condicional 1
                     {
                         Console.WriteLine("Não existe soma de números consecutivos para este número digitado!\n"); // saída 1
                         Thread.Sleep(1500);
@@ -41,34 +43,43 @@ namespace Ex021_PRL_100622
                         Thread.Sleep(800);
                         Console.Write($" + {n3}"); // Saída 2
                         Thread.Sleep(1000);
-                        Console.Write($" = {numero}\n\n"); // Saída 2
+                        Console.Write($" = {resultado}\n\n"); // Saída 2
                         Thread.Sleep(1500);
                     }
+                }
+                else
+                {
+                    Console.WriteLine("Por favor digite um número para que possa ser testado!\n"); // Saída 4
+                    Thread.Sleep(1500);
+                }
 
-                    string resposta2 = "";
-                    int contador = 3;
+                string resposta2 = "";
+                int contador = 3;
 
-                    while (resposta2 != "s" && resposta2 != "n")
+                while (resposta2 != "s")
+                {
+                    Console.WriteLine("Quer continuar [S/N]? "); // Interface 2
+                    Console.SetCursorPosition(22, contador); // Posição 2
+                    resposta2 = Console.ReadLine();
+                    resposta2 = resposta2.ToLower();
+                    resposta = resposta2;
+                    contador++;
+
+                    if (resposta != "n" && resposta != "s")
                     {
-                        Console.WriteLine("Quer continuar [S/N]? "); // Interface 2
-                        Console.SetCursorPosition(22, contador); // Posição 2
-                        resposta2 = Console.ReadLine();
-                        resposta2 = resposta.ToLower();
-                        resposta += resposta2;
-                        contador++;
-
-                        if (resposta2 == "n")
-                        {
-                            Environment.Exit(0);
-                        }
+                        contador += 2;
+                        Console.WriteLine("\nDigite apenas S ou N!");
+                    }     
+                    else if (resposta == "n")
+                    {
+                        Thread.Sleep(700);
+                        Console.WriteLine("Encerrando...");
+                        Thread.Sleep(1800);
+                        Environment.Exit(0);
                     }
-                   
                 }
             }
-            catch
-            {
-                Console.WriteLine("Por favor digite um número para que possa ser testado!"); // Saída 4 
-            }
+
             Console.ReadLine();
         }
     }
