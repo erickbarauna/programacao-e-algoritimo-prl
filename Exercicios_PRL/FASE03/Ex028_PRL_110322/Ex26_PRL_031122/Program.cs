e951c05c0afe45a14c363eb50878106e68d069c4
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-11-09 08:53:39 -0300

    estou criando uma pasta nova

--- Exercicios_PRL/FASE03/Ex028_PRL_110322/Ex26_PRL_031122/Program.cs
+++ Exercicios_PRL/FASE03/Ex028_PRL_110322/Ex26_PRL_031122/Program.cs
@@ -73,21 +73,21 @@ namespace Ex26_PRL_031122
             double Resultado = 0;
 
 
-            Console.Clear(); // Limpar Tela
+            Console.Clear(); 
             White();
             Linha();
-            Console.WriteLine("\n<Opções de Figuras>"); // Interface
+            Console.WriteLine("\n<Opções de Figuras>"); 
             Linha();
-            Console.WriteLine("\n1 - Triângulo"); // Interface - Menu
-            Console.WriteLine("2 - Quadrado"); // Interface - Menu
-            Console.WriteLine("3 - Retângulo"); // Interface - Menu
+            Console.WriteLine("\n1 - Triângulo"); 
+            Console.WriteLine("2 - Quadrado"); 
+            Console.WriteLine("3 - Retângulo"); 
             Linha();
 
             while (FiguraInt > 3 || FiguraInt < 1)
             {
                 Console.WriteLine("\nDigite a figura desejada: ");
-                Console.SetCursorPosition(26, contadorWhile1); // Posição 1
-                FiguraStr = Console.ReadLine(); // Entrada 1
+                Console.SetCursorPosition(26, contadorWhile1);
+                FiguraStr = Console.ReadLine(); 
                 Linha();
 
                 if (int.TryParse(FiguraStr, out FiguraInt))
@@ -113,31 +113,31 @@ namespace Ex26_PRL_031122
             Green();
             Console.Write(Figuras[FiguraInt - 1]);
             White();
-            Console.Write("! \nAgora selecione a opção desejada: "); // Interface
-            Console.WriteLine("\n\n1 - Área"); // Interface - Menu
-            Console.WriteLine("2 - Perimetro"); // Interface - Menu
+            Console.Write("! \nAgora selecione a opção desejada: "); 
+            Console.WriteLine("\n\n1 - Área"); 
+            Console.WriteLine("2 - Perimetro"); 
 
             while (OpcaoInt > 2 || OpcaoInt < 1)
             {
-                Console.WriteLine("\nDigite a opção: "); // Interface
+                Console.WriteLine("\nDigite a opção: "); 
                 Linha();
-                Console.SetCursorPosition(16, contadorWhile2); // Posição 2 
-                OpcaoStr = Console.ReadLine(); // Entrada 2
+                Console.SetCursorPosition(16, contadorWhile2); 
+                OpcaoStr = Console.ReadLine(); 
 
                 contadorWhile2 += 4;
 
                 if (int.TryParse(OpcaoStr, out OpcaoInt))
                 {
-                    switch (OpcaoInt) // Dispositivo de Escolha 1
+                    switch (OpcaoInt) 
                     {
                         case 1:
-                            if (FiguraInt == 1) // Condicional 1
+                            if (FiguraInt == 1) 
                             {
                                 while (ValorInt1 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor da Base: "); // Interface
-                                    Console.SetCursorPosition(24, contadorWhile2 - 2); // Posição 3
-                                    ValorStr1 = Console.ReadLine(); // Entrada 1
+                                    Console.WriteLine("\nDigite o valor da Base: "); 
+                                    Console.SetCursorPosition(24, contadorWhile2 - 2); 
+                                    ValorStr1 = Console.ReadLine(); 
 
                                     if (int.TryParse(ValorStr1, out ValorInt1))
                                     {
@@ -152,13 +152,13 @@ namespace Ex26_PRL_031122
 
                                 while (ValorInt2 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor da Altura: "); // Interface 
-                                    Console.SetCursorPosition(26, contadorWhile2 - 4); // Posição 4
-                                    ValorStr2 = Console.ReadLine(); // Entrada 2
+                                    Console.WriteLine("\nDigite o valor da Altura: "); 
+                                    Console.SetCursorPosition(26, contadorWhile2 - 4); 
+                                    ValorStr2 = Console.ReadLine(); 
 
                                    if (int.TryParse(ValorStr2, out ValorInt2))
                                     {
-                                        Resultado = (ValorInt1 * ValorInt2) / 2; // Processo 1
+                                        Resultado = (ValorInt1 * ValorInt2) / 2; 
                                     }
                                     else
                                     {
@@ -169,20 +169,20 @@ namespace Ex26_PRL_031122
 
                                 Green();
                                 Linha();
-                                Console.WriteLine($"\nÁrea do Triângulo = {Resultado}"); // Saída
+                                Console.WriteLine($"\nÁrea do Triângulo = {Resultado}");
                                 Linha();
                             }
-                            else if (FiguraInt == 2) // Outra Opção 1 - 1
+                            else if (FiguraInt == 2) 
                             {
                                 while (ValorInt1 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor do Lado: "); // Interface
-                                    Console.SetCursorPosition(24, contadorWhile2 - 2); // Posição 5
-                                    ValorStr1 = Console.ReadLine(); // Entrada 3
+                                    Console.WriteLine("\nDigite o valor do Lado: "); 
+                                    Console.SetCursorPosition(24, contadorWhile2 - 2); 
+                                    ValorStr1 = Console.ReadLine(); 
 
                                     if (int.TryParse(ValorStr1, out ValorInt1))
                                     {
-                                        Resultado = ValorInt1 * ValorInt1; // Processo 2
+                                        Resultado = ValorInt1 * ValorInt1; 
                                     }
                                     else
                                     {
@@ -192,17 +192,17 @@ namespace Ex26_PRL_031122
                                 }
                                 Green();
                                 Linha();
-                                Console.WriteLine($"\nÁrea do Quadrado = {Resultado}"); // Saida 2
+                                Console.WriteLine($"\nÁrea do Quadrado = {Resultado}"); 
                                 Linha();
 
                             }
-                            else if (FiguraInt == 3) // Outra Opção 2 - Condicional 1
+                            else if (FiguraInt == 3) 
                             {
                                 while (ValorInt1 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor da Base: "); // Interface
-                                    Console.SetCursorPosition(24, contadorWhile2 - 2); // Posição 6
-                                    ValorStr1 = Console.ReadLine(); // Entrada 4
+                                    Console.WriteLine("\nDigite o valor da Base: "); 
+                                    Console.SetCursorPosition(24, contadorWhile2 - 2); 
+                                    ValorStr1 = Console.ReadLine(); 
 
                                     if (int.TryParse(ValorStr1, out ValorInt1))
                                     {
@@ -217,13 +217,13 @@ namespace Ex26_PRL_031122
 
                                 while (ValorInt2 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor da Altura: "); // Interface
-                                    Console.SetCursorPosition(26, contadorWhile2 - 4); // Posição 7
-                                    ValorStr2 = Console.ReadLine(); // Entrada 5
+                                    Console.WriteLine("\nDigite o valor da Altura: "); 
+                                    Console.SetCursorPosition(26, contadorWhile2 - 4); 
+                                    ValorStr2 = Console.ReadLine(); 
 
                                     if (int.TryParse(ValorStr2, out ValorInt2))
                                     {
-                                        Resultado = (ValorInt1 * ValorInt2); // Processo 3
+                                        Resultado = (ValorInt1 * ValorInt2); 
                                     }
                                     else
                                     {
@@ -234,20 +234,20 @@ namespace Ex26_PRL_031122
 
                                 Green();
                                 Linha();
-                                Console.WriteLine($"\nÁrea do Retângulo = {Resultado}"); // Saída 3
+                                Console.WriteLine($"\nÁrea do Retângulo = {Resultado}"); 
                                 Linha();
                             }
 
                             break;
 
                         case 2:
-                            if (FiguraInt == 1) // Condicional 2
+                            if (FiguraInt == 1) 
                             {
                                 while (ValorInt1 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor do 1° lado: "); // Interface
-                                    Console.SetCursorPosition(27, contadorWhile2 - 2); // Posição 8
-                                    ValorStr1 = Console.ReadLine(); // Entrada 5
+                                    Console.WriteLine("\nDigite o valor do 1° lado: "); 
+                                    Console.SetCursorPosition(27, contadorWhile2 - 2); 
+                                    ValorStr1 = Console.ReadLine(); 
 
                                     if (int.TryParse(ValorStr1, out ValorInt1))
                                     {
@@ -262,9 +262,9 @@ namespace Ex26_PRL_031122
 
                                 while (ValorInt2 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor do 2° lado: "); // Interface
-                                    Console.SetCursorPosition(27, contadorWhile2 - 4); // Posição 9
-                                    ValorStr2 = Console.ReadLine(); // Entrada 6
+                                    Console.WriteLine("\nDigite o valor do 2° lado: "); 
+                                    Console.SetCursorPosition(27, contadorWhile2 - 4); 
+                                    ValorStr2 = Console.ReadLine(); 
 
                                     if (int.TryParse(ValorStr2, out ValorInt2))
                                     {
@@ -279,13 +279,13 @@ namespace Ex26_PRL_031122
 
                                 while (ValorInt3 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor do 3° lado: "); // Interface
-                                    Console.SetCursorPosition(27, contadorWhile2 - 6); // Posição 10
-                                    ValorStr3 = Console.ReadLine(); // Entrada 7
+                                    Console.WriteLine("\nDigite o valor do 3° lado: "); 
+                                    Console.SetCursorPosition(27, contadorWhile2 - 6); 
+                                    ValorStr3 = Console.ReadLine(); 
 
                                     if (int.TryParse(ValorStr3, out ValorInt3))
                                     {
-                                        Resultado = ValorInt1 + ValorInt2 + ValorInt3; // Processo 4
+                                        Resultado = ValorInt1 + ValorInt2 + ValorInt3;
                                     }
                                     else
                                     {
@@ -296,20 +296,20 @@ namespace Ex26_PRL_031122
 
                                 Green();
                                 Linha();
-                                Console.WriteLine($"\nPerimêtro do Triângulo = {Resultado} "); // Saída 4
+                                Console.WriteLine($"\nPerimêtro do Triângulo = {Resultado} "); 
                                 Linha();
                             }
-                            else if (FiguraInt == 2)  // Outra Opção 1 - Condicional 2
+                            else if (FiguraInt == 2)  
                             {
                                 while (ValorInt1 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor do Lado: "); // Interface 
-                                    Console.SetCursorPosition(24, contadorWhile2 - 2); // Posição 11
-                                    ValorStr1 = Console.ReadLine(); // Entrada 8
+                                    Console.WriteLine("\nDigite o valor do Lado: ");
+                                    Console.SetCursorPosition(24, contadorWhile2 - 2); 
+                                    ValorStr1 = Console.ReadLine(); 
 
                                     if (int.TryParse(ValorStr1, out ValorInt1))
                                     {
-                                        Resultado = ValorInt1 * 4; // Processo 5
+                                        Resultado = ValorInt1 * 4; 
                                     }
                                     else
                                     {
@@ -320,16 +320,16 @@ namespace Ex26_PRL_031122
 
                                 Green();
                                 Linha();
-                                Console.WriteLine($"\nPerimêtro do Quadrado = {Resultado}"); // Saída 5
+                                Console.WriteLine($"\nPerimêtro do Quadrado = {Resultado}"); 
                                 Linha();
                             }
-                            else if (FiguraInt == 3) // Outra Opção 2 - COndicional 2
+                            else if (FiguraInt == 3) 
                             {
                                 while (ValorInt1 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor da Base: "); // Interface
-                                    Console.SetCursorPosition(24, contadorWhile2 - 2); // Posição 12
-                                    ValorStr1 = Console.ReadLine(); // Entrada 9
+                                    Console.WriteLine("\nDigite o valor da Base: "); 
+                                    Console.SetCursorPosition(24, contadorWhile2 - 2); 
+                                    ValorStr1 = Console.ReadLine();
 
                                     if (int.TryParse(ValorStr1, out ValorInt1))
                                     {
@@ -344,13 +344,13 @@ namespace Ex26_PRL_031122
                                 
                                 while (ValorInt2 == 0)
                                 {
-                                    Console.WriteLine("\nDigite o valor da Altura: "); // Interface
-                                    Console.SetCursorPosition(26, contadorWhile2 - 4); // Posição 13
-                                    ValorStr2 = Console.ReadLine(); // Entrada 10
+                                    Console.WriteLine("\nDigite o valor da Altura: "); 
+                                    Console.SetCursorPosition(26, contadorWhile2 - 4); 
+                                    ValorStr2 = Console.ReadLine(); 
                                     
                                     if (int.TryParse(ValorStr2, out ValorInt2))
                                     {
-                                        Resultado = (ValorInt1 * ValorInt2) + (ValorInt2 * 2); // Processo 6
+                                        Resultado = (ValorInt1 * ValorInt2) + (ValorInt2 * 2); 
                                     }
                                     else
                                     {
@@ -361,16 +361,16 @@ namespace Ex26_PRL_031122
                                 
                                 Green();
                                 Linha();
-                                Console.WriteLine($"\nPerimetro do Retângulo = {Resultado}"); // Saída 6
+                                Console.WriteLine($"\nPerimetro do Retângulo = {Resultado}"); 
                                 Linha();
                             }
                             break;
-                        default: // Opção Padrão
+                        default: 
                             Red();
-                            Console.WriteLine("\nDigite uma Opção Válida!"); // Saída 7
+                            Console.WriteLine("\nDigite uma Opção Válida!");
                             White();
                             Linha();
-                            Resultado = 0; // Processo 7
+                            Resultado = 0;
                             break;
                     }
                 }
