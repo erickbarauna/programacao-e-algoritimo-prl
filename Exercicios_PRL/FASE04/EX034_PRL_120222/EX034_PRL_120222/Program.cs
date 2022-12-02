e25b44bd55249cef87781db3215e9d8fdafd4020
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-12-02 19:16:03 -0300

    ex34
    escreve em arquivo

---
+++ Exercicios_PRL/FASE04/EX034_PRL_120222/EX034_PRL_120222/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // Permite abrir e fechar arquivo
+
+namespace EX034_PRL_120222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1: Escreve uma linha para um arquivo novo
+            using (StreamWriter writer = new StreamWriter("C:\\Arquivos\\Ex34.txt", true))
+            {
+                Console.Write("Digite um texto: ");
+                Console.SetCursorPosition(17, 0);
+                writer.WriteLine(Console.ReadLine());
+            }
+            // 2: Anexa uma linha no arquivo novo
+            using (StreamWriter writer = new StreamWriter(@"C:\\Arquivos\\Ex34.txt", true))
+            {
+                writer.WriteLine("Resultado da gravação do texto");
+            }
+        }
+    }
+}
