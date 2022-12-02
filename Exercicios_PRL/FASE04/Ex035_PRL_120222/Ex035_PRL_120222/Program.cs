6cadbf092aafa7255c197dc2f16373d90957d29b
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-12-02 19:08:39 -0300

    ex035
    grava arquivo dentro do projeto

---
+++ Exercicios_PRL/FASE04/Ex035_PRL_120222/Ex035_PRL_120222/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // Permite abrir e fechar arquivo
+
+namespace Ex035_PRL_120222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var writer = new StreamWriter("Ex35.txt"))
+            // Grava arquivo dentro do projeto 
+            {
+                // Percorre o laço
+                for (int i = 0; i < 10; i++)
+                {
+                    // Escreve uma string formatada no arquivo
+                    writer.Write("{0:0.0}", i);
+                }
+            }
+        }
+    }
+}
