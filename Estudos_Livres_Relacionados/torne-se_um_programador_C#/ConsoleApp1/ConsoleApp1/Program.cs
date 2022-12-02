47e7d54167201b71988ceb86302d8cd43c84c706
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-12-02 18:48:07 -0300

    ex037
    validação de cpf

---
+++ Estudos_Livres_Relacionados/torne-se_um_programador_C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Matriz = new int[5, 4];
+
+            Matriz[0,0] =  1;
+            Matriz[0,1] =  2;
+            Matriz[0,2] =  3;
+            Matriz[0,3] =  4;
+
+            Matriz[1,0] =  5;
+            Matriz[1,1] =  6;
+            Matriz[1,2] =  7;
+            Matriz[1,3] =  8;
+
+            Matriz[2,0] =  9;
+            Matriz[2,1] =  10;
+            Matriz[2,2] =  11;
+            Matriz[2,3] =  12;
+
+            Matriz[3,0] =  13;
+            Matriz[3,1] =  14;
+            Matriz[3,2] =  15;
+            Matriz[3,3] =  16;
+
+            Matriz[4,0] =  17;
+            Matriz[4,1] =  18;
+            Matriz[4,2] =  19;
+            Matriz[4,3] =  20;
+
+            for (int i = 0; i < 16; i++)
+            {
+                Console.WriteLine(Matriz[i, i]);
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicios_PRL/FASE04/Ex037_PRL_120222/Ex037_PRL_120222/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex037_PRL_120222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string CPF, POS; // Variveis de Texto entrada
+            int Tamanho, Verificar = 0, Resto = 0, Multiplicador = 11, Soma = 0; // Variveis de Saida
+            int[] num = new int[14]; // Vetor inteiro de 14 Indices
+
+            Console.Clear(); // Limpa Tela
+            Console.WriteLine("Digite um CPF: "); //Interface 1
+            Console.SetCursorPosition (15, 0); // Posição 1
+
+            CPF = (Console.ReadLine()); // Entrada 1 Tamanho CPF.Length; // Processo 1
+            Tamanho = CPF.Length; // Processo 1
+
+            for (int i = 0; i < Tamanho; i++) // Laçol Para
+            {
+                POS = CPF.Substring(i, 1); // Processo 2
+
+                if (POS == "." || POS == "-") // Condicional 1
+                {
+                    num[i] = 0; // Processo 3
+                }
+                else // Negação Condicional 1{
+                {
+                    num[i] = int.Parse(POS) * Multiplicador; // Processo 4
+                    Multiplicador--; // Processo 5
+                }
+            }
+
+            for (int j = 0; j < 13; j++) // Laço 2 - Para
+            {
+                Soma = Soma + num[j]; // Processo 6
+            }
+
+            Resto = Soma % 11; // Processo 7
+            Verificar = 11 - Resto; // Processo 8
+
+            if (Resto <= 1) Verificar = 0; //Condicional 2 - Processo 9
+
+            if (Verificar == num[13]) // Condicional 3
+            {
+                Console.WriteLine("CPF: " + CPF + " Válido"); // Saída 1
+            }
+            else // Negação Condicional 3
+            {
+                Console.WriteLine("CPF: " + CPF + " Inválido"); // Saída 2
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
