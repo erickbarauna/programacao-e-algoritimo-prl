2f5b0a318e85ba11619a86412fec9a6bd73304ff
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-12-03 06:06:22 -0300

    ex031
    toUpper ToLower

---
+++ Exercicios_PRL/FASE04/Ex031_PRL_120322/Ex031_PRL_120322/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex031_PRL_120322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Palavra; // Variável de Texto - Entrada
+            string Maiusculo, Minusculo; // Variavel de Texto - Saída
+
+            Console.Clear();
+            Console.WriteLine("Digite uma palavra: "); // Interface 1
+            Console.SetCursorPosition(21, 0); // Posição 1
+            Palavra = Console.ReadLine(); // Entrada 1
+
+            Maiusculo = Palavra.ToUpper(); // Processo 1
+            Minusculo = Palavra.ToUpper(); // Processo 2
+
+            Console.WriteLine("Maiusculo: " + Maiusculo); // Saída 1
+            Console.WriteLine("Minusculo: " + Minusculo); // Saída 2
+
+            Console.ReadLine();
+        }
+    }
+}
