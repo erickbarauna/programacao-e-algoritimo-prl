84aa10ec0f908e90e22a3700b510786f8011ff88
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-12-03 06:10:12 -0300

    ex032
    comando length

---
+++ Exercicios_PRL/FASE04/Ex032_PRL_120322/Ex032_PRL_120322/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex032_PRL_120322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Palavra; // Variável de Texto - Entrada 
+            int Tamanho; // Variável inteira - Saída 
+
+            Console.Clear();
+            Console.WriteLine("Digite uma palavra: "); // Interface 1
+            Console.SetCursorPosition(21, 0); // Posição 1
+            Palavra = Console.ReadLine(); // Entrada 1
+
+            Tamanho = Palavra.Length; // Processso 1
+
+            Console.WriteLine("Número de caracteres: " + Tamanho); // Saída 1
+
+            Console.ReadLine();
+        }
+    }
+}
