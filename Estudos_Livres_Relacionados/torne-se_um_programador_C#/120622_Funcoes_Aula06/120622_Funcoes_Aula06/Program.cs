252d13689b42c94b25afa0ff5b66f03847add7bd
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-12-06 07:07:05 -0300

    Aula - 06 - FUNÇÕES

---
+++ Estudos_Livres_Relacionados/torne-se_um_programador_C#/120622_Funcoes_Aula06/120622_Funcoes_Aula06/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _120622_Funcoes_Aula06
+{
+    class Program
+    {
+        static int CalcularSoma()
+        {
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+
+            return c;
+        }
+
+        public static void MostrarMensagemNaTela()
+        {
+            Console.WriteLine("Hi guys!");
+        }
+
+        public static void Tabuada(int numero)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{numero} X {i} = {numero*i}");
+            }
+            Console.WriteLine("==========");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalcularSoma());
+            MostrarMensagemNaTela();
+
+            Tabuada(7);
+            Tabuada(8);
+            Tabuada(9);
+
+            Console.ReadLine();
+        }
+    }
+}
--- Estudos_Livres_Relacionados/torne-se_um_programador_C#/120622_Funcoes_Aula06/120622_Funcoes_Aula06/Properties/AssemblyInfo.cs
+++ Estudos_Livres_Relacionados/torne-se_um_programador_C#/120622_Funcoes_Aula06/120622_Funcoes_Aula06/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("120622_Funcoes_Aula06")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("120622_Funcoes_Aula06")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("7fdd00f9-fba6-4057-a39c-ba490d5e5b0b")]
+[assembly: Guid("70cafd4f-f027-42d9-b9f0-7ad18056af54")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
