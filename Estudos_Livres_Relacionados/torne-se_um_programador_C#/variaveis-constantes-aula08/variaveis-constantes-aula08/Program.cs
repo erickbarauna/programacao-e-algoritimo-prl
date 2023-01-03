c4fb0e18f360b99db21c4f96eba739f182d5331a
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2023-01-03 07:15:02 -0300

    aula 08

---
+++ Estudos_Livres_Relacionados/torne-se_um_programador_C#/variaveis-constantes-aula08/variaveis-constantes-aula08/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variaveis_constantes_aula08
+{
+    internal class Program
+    {
+        public const int SAIDA_PROGRAMA = 1;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Digite {SAIDA_PROGRAMA} para sair do programa");
+                int valor = int.Parse(Console.ReadLine());
+
+                if(SAIDA_PROGRAMA == valor)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
